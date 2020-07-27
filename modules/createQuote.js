7651a26cd9ff075a7073549e1629eeafb9e7b028
swastikmohapatra <mohapatra.swasti@gmail.com>
2020-07-27 15:02:29 +0530

    Add files via upload

--- modules/createQuote.js
+++ modules/createQuote.js
@@ -16,51 +16,33 @@ exports.execute = (req, res) => {
         params = req.body.text.split(":"),
         Name = params[0],
         OpportunityId = params[1];
-		
-		q = "SELECT Id, Name FROM Opportunity WHERE Name = '" + OpportunityId + "'";
-		
-		
-		 force.query(oauthObj, q)
+		force.create(oauthObj, "Quote",
+        {
+            Name: Name,
+            OpportunityId : "0064P00000lfN28"
+           
+
+        })
         .then(data => {
-				 let Oppty = JSON.parse(data).records;
-				 force.create(oauthObj, "Quote",
-				 {
-					Name: Name,
-					OpportunityId : Oppty.Id
-				   
-
-				 })
-				.then(data => {
-							let fields = [];
-							fields.push({title: "OpportunityId", value: OpportunityId, short:false});
-							fields.push({title: "Name", value: Name, short:false});
-							fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + data.id, short:false});
-							let message = {
-								text: "A new Quote has been created:",
-								attachments: [
-									{color: "#F2CF5B", fields: fields}
-								]
-							};
-							res.json(message);
-				})
-				.catch((error) => {
-						if (error.code == 401) {
-							res.send('Visit this URL to login to Salesforce: https://${req.hostname}/login/' + slackUserId);
-
-						} else {
-							res.send("An error as occurred");
-						}
-				});
-		})
-		.catch(error => {
+            let fields = [];
+            fields.push({title: "OpportunityId", value: OpportunityId, short:false});
+            fields.push({title: "Name", value: Name, short:false});
+            fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + data.id, short:false});
+            let message = {
+                text: "A new Quote has been created:",
+                attachments: [
+                    {color: "#F2CF5B", fields: fields}
+                ]
+            };
+            res.json(message);
+        })
+        .catch((error) => {
             if (error.code == 401) {
-                res.send('Visit this URL to login to Salesforce: https:');
+                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
+
             } else {
                 res.send("An error as occurred");
             }
         });
-		
-		
-		
 
 };
