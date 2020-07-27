8295e26d3448580131a95e6899ee1959f08d052a
swastikmohapatra <mohapatra.swasti@gmail.com>
2020-07-27 17:24:47 +0530

    Update createQuote.js

--- modules/createQuote.js
+++ modules/createQuote.js
@@ -51,7 +51,7 @@ exports.execute = (req, res) => {
 				})
 				.catch((error) => {
 						if (error.code == 401) {
-							res.send('Visit this URL to login to Salesforce: https://${req.hostname}/login/' + slackUserId);
+							  res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
 
 						} else {
 							res.send("An error as occurred");
@@ -60,7 +60,7 @@ exports.execute = (req, res) => {
 		})
 		.catch(error => {
             if (error.code == 401) {
-               res.send('Visit this URL to login to Salesforce: https://${req.hostname}/login/' + slackUserId);
+                res.send(`Visit this URL to login to Salesforce: https://${req.hostname}/login/` + slackUserId);
             } else {
                 res.send("An error as occurred");
             }
