e9c0cb02f69ee5f4979bd1f1abd70a2f0a83135c
swastikmohapatra <mohapatra.swasti@gmail.com>
2020-07-27 15:13:11 +0530

    Add files via upload

---
+++ modules/createQuote.js
@@ -0,0 +1,72 @@
+"use strict";
+
+let auth = require("./slack-salesforce-auth"),
+    force = require("./force"),
+    CREATEQUOTE_TOKEN = process.env.SLACK_CREATEQUOTE_TOKEN;
+
+exports.execute = (req, res) => {
+
+    if (req.body.token != CREATEQUOTE_TOKEN) {
+        res.send("Invalid token");
+        return;
+    }
+
+    let slackUserId = req.body.user_id,
+        oauthObj = auth.getOAuthObject(slackUserId),
+        params = req.body.text.split(":"),
+        Name = params[0],
+        OpportunityId = params[1];
+		var opptyId;
+		q = "SELECT Id, Name FROM Opportunity WHERE Name = '" + OpportunityId + "'";
+		
+		
+		force.query(oauthObj, q)
+        .then(data => {
+				 let Oppty = JSON.parse(data).records;
+				 if (Oppty && Oppty.length>0) {
+						 Oppty.forEach(function(opp) {
+						  opptyId =  opp.Id;
+						  
+						});
+                }
+     			 force.create(oauthObj, "Quote",
+				 {
+					Name: Name,
+					OpportunityId : opptyId
+				   
+
+				 })
+				.then(data => {
+							let fields = [];
+							fields.push({title: "OpportunityId", value: OpportunityId, short:false});
+							fields.push({title: "Name", value: Name, short:false});
+							fields.push({title: "Open in Salesforce:", value: oauthObj.instance_url + "/" + data.id, short:false});
+							let message = {
+								text: "A new Quote has been created:",
+								attachments: [
+									{color: "#F2CF5B", fields: fields}
+								]
+							};
+							res.json(message);
+				})
+				.catch((error) => {
+						if (error.code == 401) {
+							res.send('Visit this URL to login to Salesforce: https://${req.hostname}/login/' + slackUserId);
+
+						} else {
+							res.send("An error as occurred");
+						}
+				});
+		})
+		.catch(error => {
+            if (error.code == 401) {
+                res.send('Visit this URL to login to Salesforce: https:');
+            } else {
+                res.send("An error as occurred");
+            }
+        });
+		
+		
+		
+
+};
